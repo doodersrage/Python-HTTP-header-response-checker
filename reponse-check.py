bdfe2b68179b0edc784940ece45bcc1101c5082f
Robert McDowell <rob@studiocenter.com>
2014-06-07 18:24:44 -0400

    init commit

---
+++ reponse-check.py
@@ -0,0 +1,54 @@
+import requests
+
+SAVEFILE = "results.txt"
+FILENAME = "pages.txt"
+REPLACE = "http://virginiabeachwebdevelopment.com/"
+NEWURL = "http://virginiabeachwebdevelopment.com/"
+#clear output file
+f = open(SAVEFILE, 'r+')
+f.truncate()
+
+# init counters
+REDCNT = 0
+NTFNDCNT = 0
+OKCNT = 0
+
+# open results file for writing
+fw = open(SAVEFILE,'w')
+# open link list
+f=open(FILENAME,'r')
+# walk through link file
+for line in f.readlines():
+
+    newlink = line.strip().replace(REPLACE, NEWURL);
+    # get header response
+    conn = requests.get(newlink)
+
+    response = conn.status_code
+    # record redirected pages
+    if response == 301 or response == 302:
+        newpage = conn.url
+        REDCNT += 1
+    else:
+        newpage=''
+
+    # record missing pages
+    if response == 404:
+        NTFNDCNT += 1
+    # record found pages
+    if response == 200:
+        OKCNT += 1
+    
+    # write to output file
+    results = str(response) + "\t" + newlink + "\t" + newpage + "\n"
+    fw.write(results)
+
+# close results file
+fw.close()
+# close link list file
+f.close()
+
+print "Redirects:" + str(REDCNT)
+print "200 OK:" + str(OKCNT)
+print "404 not found:" + str(NTFNDCNT)
+print "DONE!"
